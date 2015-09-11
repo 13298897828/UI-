f729bb7d9b5b5ca6cf8fb58eee5fc695d4f5d736
Tianqi Zhang <1120404010@qq.com>
2015-09-11 09:02:10 +0800

    Initial Commit

---
+++ Lesson_10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_10
+//
+//  Created by 张天琦 on 15/9/11.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
